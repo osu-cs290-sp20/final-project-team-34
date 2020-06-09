d4ad7d02431f3f7374a52d8411da5757db2db222
mark <mdeane84@yahoo.com>
2020-06-09 15:12:27 -0700

    before merge

--- public/defaultTemplate.js
+++ public/defaultTemplate.js
@@ -8,32 +8,32 @@ templates['default'] = template({"compiler":[8,">= 4.3.0"],"main":function(conta
         return undefined
     };
 
-  return "<div>\r\n  <p>log "
-    + alias4(((helper = (helper = lookupProperty(helpers,"logNumber") || (depth0 != null ? lookupProperty(depth0,"logNumber") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"logNumber","hash":{},"data":data,"loc":{"start":{"line":2,"column":9},"end":{"line":2,"column":22}}}) : helper)))
-    + "</p>\r\n  <p>Handler: "
-    + alias4(((helper = (helper = lookupProperty(helpers,"handler") || (depth0 != null ? lookupProperty(depth0,"handler") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"handler","hash":{},"data":data,"loc":{"start":{"line":3,"column":14},"end":{"line":3,"column":25}}}) : helper)))
-    + "</p>\r\n  <p>Dog: "
-    + alias4(((helper = (helper = lookupProperty(helpers,"dog") || (depth0 != null ? lookupProperty(depth0,"dog") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"dog","hash":{},"data":data,"loc":{"start":{"line":4,"column":10},"end":{"line":4,"column":17}}}) : helper)))
-    + "</p>\r\n  <div>\r\n    <span>Date: "
-    + alias4(((helper = (helper = lookupProperty(helpers,"date") || (depth0 != null ? lookupProperty(depth0,"date") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"date","hash":{},"data":data,"loc":{"start":{"line":6,"column":16},"end":{"line":6,"column":24}}}) : helper)))
-    + "</span>\r\n    <span>Phase of Day: "
-    + alias4(((helper = (helper = lookupProperty(helpers,"phase") || (depth0 != null ? lookupProperty(depth0,"phase") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"phase","hash":{},"data":data,"loc":{"start":{"line":7,"column":24},"end":{"line":7,"column":33}}}) : helper)))
-    + "</span>\r\n    <span>Duration: "
-    + alias4(((helper = (helper = lookupProperty(helpers,"duration") || (depth0 != null ? lookupProperty(depth0,"duration") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"duration","hash":{},"data":data,"loc":{"start":{"line":8,"column":20},"end":{"line":8,"column":32}}}) : helper)))
-    + "</span>\r\n  </div>\r\n  <div>\r\n    <span>weather: "
-    + alias4(((helper = (helper = lookupProperty(helpers,"weather") || (depth0 != null ? lookupProperty(depth0,"weather") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"weather","hash":{},"data":data,"loc":{"start":{"line":11,"column":19},"end":{"line":11,"column":30}}}) : helper)))
-    + "</span>\r\n    <span>wind: "
-    + alias4(((helper = (helper = lookupProperty(helpers,"wind") || (depth0 != null ? lookupProperty(depth0,"wind") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"wind","hash":{},"data":data,"loc":{"start":{"line":12,"column":16},"end":{"line":12,"column":24}}}) : helper)))
-    + "</span>\r\n  </div>\r\n  <div>\r\n    <span>Problem Type: "
-    + alias4(((helper = (helper = lookupProperty(helpers,"problem") || (depth0 != null ? lookupProperty(depth0,"problem") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"problem","hash":{},"data":data,"loc":{"start":{"line":15,"column":24},"end":{"line":15,"column":35}}}) : helper)))
-    + "</span>\r\n    <span>Age of Trail: "
-    + alias4(((helper = (helper = lookupProperty(helpers,"trail") || (depth0 != null ? lookupProperty(depth0,"trail") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"trail","hash":{},"data":data,"loc":{"start":{"line":16,"column":24},"end":{"line":16,"column":33}}}) : helper)))
-    + "</span>\r\n  </div>\r\n  <p>Environment:</p>\r\n  <p>"
-    + alias4(((helper = (helper = lookupProperty(helpers,"environment") || (depth0 != null ? lookupProperty(depth0,"environment") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"environment","hash":{},"data":data,"loc":{"start":{"line":19,"column":5},"end":{"line":19,"column":20}}}) : helper)))
-    + "</p>\r\n\r\n  <p>Performance Analysis:</p>\r\n  <p>"
-    + alias4(((helper = (helper = lookupProperty(helpers,"performance") || (depth0 != null ? lookupProperty(depth0,"performance") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"performance","hash":{},"data":data,"loc":{"start":{"line":22,"column":5},"end":{"line":22,"column":20}}}) : helper)))
-    + "</p>\r\n\r\n  <p>Training Goals:</p>\r\n  <p>"
-    + alias4(((helper = (helper = lookupProperty(helpers,"goals") || (depth0 != null ? lookupProperty(depth0,"goals") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"goals","hash":{},"data":data,"loc":{"start":{"line":25,"column":5},"end":{"line":25,"column":14}}}) : helper)))
-    + "</p>\r\n</div>\r\n";
+  return "<article class=\"log\">\r\n  <div class=\"log-content\">\r\n    <p>log "
+    + alias4(((helper = (helper = lookupProperty(helpers,"logNumber") || (depth0 != null ? lookupProperty(depth0,"logNumber") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"logNumber","hash":{},"data":data,"loc":{"start":{"line":3,"column":11},"end":{"line":3,"column":24}}}) : helper)))
+    + "</p>\r\n    <p>Handler: "
+    + alias4(((helper = (helper = lookupProperty(helpers,"handler") || (depth0 != null ? lookupProperty(depth0,"handler") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"handler","hash":{},"data":data,"loc":{"start":{"line":4,"column":16},"end":{"line":4,"column":27}}}) : helper)))
+    + "</p>\r\n    <p>Dog: "
+    + alias4(((helper = (helper = lookupProperty(helpers,"dog") || (depth0 != null ? lookupProperty(depth0,"dog") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"dog","hash":{},"data":data,"loc":{"start":{"line":5,"column":12},"end":{"line":5,"column":19}}}) : helper)))
+    + "</p>\r\n    <div>\r\n      <span>Date: "
+    + alias4(((helper = (helper = lookupProperty(helpers,"date") || (depth0 != null ? lookupProperty(depth0,"date") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"date","hash":{},"data":data,"loc":{"start":{"line":7,"column":18},"end":{"line":7,"column":26}}}) : helper)))
+    + "</span>\r\n      <span>Phase of Day: "
+    + alias4(((helper = (helper = lookupProperty(helpers,"phase") || (depth0 != null ? lookupProperty(depth0,"phase") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"phase","hash":{},"data":data,"loc":{"start":{"line":8,"column":26},"end":{"line":8,"column":35}}}) : helper)))
+    + "</span>\r\n      <span>Duration: "
+    + alias4(((helper = (helper = lookupProperty(helpers,"duration") || (depth0 != null ? lookupProperty(depth0,"duration") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"duration","hash":{},"data":data,"loc":{"start":{"line":9,"column":22},"end":{"line":9,"column":34}}}) : helper)))
+    + "</span>\r\n    </div>\r\n    <div>\r\n      <span>weather: "
+    + alias4(((helper = (helper = lookupProperty(helpers,"weather") || (depth0 != null ? lookupProperty(depth0,"weather") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"weather","hash":{},"data":data,"loc":{"start":{"line":12,"column":21},"end":{"line":12,"column":32}}}) : helper)))
+    + "</span>\r\n      <span>wind: "
+    + alias4(((helper = (helper = lookupProperty(helpers,"wind") || (depth0 != null ? lookupProperty(depth0,"wind") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"wind","hash":{},"data":data,"loc":{"start":{"line":13,"column":18},"end":{"line":13,"column":26}}}) : helper)))
+    + "</span>\r\n    </div>\r\n    <div>\r\n      <span>Problem Type: "
+    + alias4(((helper = (helper = lookupProperty(helpers,"problem") || (depth0 != null ? lookupProperty(depth0,"problem") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"problem","hash":{},"data":data,"loc":{"start":{"line":16,"column":26},"end":{"line":16,"column":37}}}) : helper)))
+    + "</span>\r\n      <span>Age of Trail: "
+    + alias4(((helper = (helper = lookupProperty(helpers,"trail") || (depth0 != null ? lookupProperty(depth0,"trail") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"trail","hash":{},"data":data,"loc":{"start":{"line":17,"column":26},"end":{"line":17,"column":35}}}) : helper)))
+    + "</span>\r\n    </div>\r\n    <p>Environment:</p>\r\n    <p>"
+    + alias4(((helper = (helper = lookupProperty(helpers,"environment") || (depth0 != null ? lookupProperty(depth0,"environment") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"environment","hash":{},"data":data,"loc":{"start":{"line":20,"column":7},"end":{"line":20,"column":22}}}) : helper)))
+    + "</p>\r\n\r\n    <p>Performance Analysis:</p>\r\n    <p>"
+    + alias4(((helper = (helper = lookupProperty(helpers,"performance") || (depth0 != null ? lookupProperty(depth0,"performance") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"performance","hash":{},"data":data,"loc":{"start":{"line":23,"column":7},"end":{"line":23,"column":22}}}) : helper)))
+    + "</p>\r\n\r\n    <p>Training Goals:</p>\r\n    <p>"
+    + alias4(((helper = (helper = lookupProperty(helpers,"goals") || (depth0 != null ? lookupProperty(depth0,"goals") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"goals","hash":{},"data":data,"loc":{"start":{"line":26,"column":7},"end":{"line":26,"column":16}}}) : helper)))
+    + "</p>\r\n  </div>\r\n</article>";
 },"useData":true});
 })();
--- public/logsPageTemplate.js
+++ public/logsPageTemplate.js
@@ -8,7 +8,7 @@ templates['logsPage'] = template({"1":function(container,depth0,helpers,partials
         return undefined
     };
 
-  return ((stack1 = container.invokePartial(lookupProperty(partials,"default"),depth0,{"name":"default","data":data,"indent":"  ","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "");
+  return ((stack1 = container.invokePartial(lookupProperty(partials,"default"),depth0,{"name":"default","data":data,"indent":"    ","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "");
 },"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, lookupProperty = container.lookupProperty || function(parent, propertyName) {
         if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
@@ -17,7 +17,8 @@ templates['logsPage'] = template({"1":function(container,depth0,helpers,partials
         return undefined
     };
 
-  return "<h1>BrownDog Toolkit</h1>\r\n<hr>\r\n<p style=\"font-family:'Comic Sans MS'\">This is the page. This is it. Just some text.</p>\r\n"
-    + ((stack1 = lookupProperty(helpers,"each").call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? lookupProperty(depth0,"logs") : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":4,"column":0},"end":{"line":6,"column":9}}})) != null ? stack1 : "");
+  return "<h1 class=\"logs-title\">Logs</h1>\r\n\r\n\r\n\r\n<div class=\"display-log-container\">\r\n\r\n"
+    + ((stack1 = lookupProperty(helpers,"each").call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? lookupProperty(depth0,"logs") : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":7,"column":2},"end":{"line":9,"column":11}}})) != null ? stack1 : "")
+    + "</div>";
 },"usePartial":true,"useData":true});
 })();
