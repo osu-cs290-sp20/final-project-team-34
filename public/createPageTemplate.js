c205b8810c9bdc424d9535f055572f6288da65dc
mark <mdeane84@yahoo.com>
2020-06-10 12:57:19 -0700

    Appended CSS

--- public/createPageTemplate.js
+++ public/createPageTemplate.js
@@ -8,8 +8,8 @@ templates['createPage'] = template({"compiler":[8,">= 4.3.0"],"main":function(co
         return undefined
     };
 
-  return "<script src=\"create.js\" defer></script>\r\n\r\n<h1 class=\"logs-title\">New Logs</h1>\r\n\r\n"
+  return "<script src=\"create.js\" defer></script>\r\n\r\n<h1 id=\"new-log-title\" class=\"logs-title\">New Logs</h1>\r\n\r\n"
     + ((stack1 = container.invokePartial(lookupProperty(partials,"defaultCreate"),depth0,{"name":"defaultCreate","data":data,"helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
-    + "\r\n<input class=\"log-submit\" id=\"btn-submit\" type=\"button\" value=\"Submit\"></input>\r\n\r\n\r\n";
+    + "\r\n<input class=\"log-submit\" id=\"btn-submit\" type=\"button\" value=\"Submit\"></input>\r\n";
 },"usePartial":true,"useData":true});
 })();
